5982474c12555d6579742fbbac03c82183f46530
BlackCNP <mrx30050@gmail.com>
2022-09-17 11:00:31 +0300

    C Sharp

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Lab2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int nn;
+            int nk;
+            int k;
+            double res = 0;
+            nn = int.Parse(Console.ReadLine());
+            nk = int.Parse(Console.ReadLine());
+            for(k=nn;k<=nk;k++)
+            {
+                res+=2* Math.Pow(k,2);
+            }
+           
+            Console.WriteLine($"res={res}");
+           
+
+
+        }
+
+    }
+}
---
+++ Plus in Sharp/Plus in Sharp/Program.cs
@@ -0,0 +1,40 @@
+﻿ using System;
+namespace T1
+{
+    class Program           //Ввести числа, знайти Max,min,кількість
+    {
+        static void Main()
+        {
+            int max = 0, min = 1, i = 0, count = 0, q = 0;
+            
+            while (true)
+            {
+                Console.WriteLine("Введіть Числа: ");
+                i = Convert.ToInt32(Console.ReadLine());
+
+
+                if (i < min)
+                {
+                    min = i;
+                }
+                if (i > max)
+                {
+                    max = i;
+                }
+                count += (i % 1) + 1;
+                q = max - min;
+                if (i == 0)
+                {
+                    break;
+                }
+
+            }
+            Console.WriteLine($"Скiльки=  {count - 1}");
+            Console.WriteLine($"Max=  {max}");
+            Console.WriteLine($"Min=  {min}");
+            Console.WriteLine($"Рiзниця=  {q}");
+
+        }
+
+    }
+}
