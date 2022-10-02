d33b67406802181a08cbb1c7d8e0ce578f88debe
BlackCNP <mrx30050@gmail.com>
2022-10-02 11:14:18 +0300

    LAb 3,4

---
+++ Lab3_V9/Lab3_V9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Lab3_V9  //Варіант 9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, sum;
+            Console.WriteLine("Напишiть число");
+            d = Convert.ToInt32(Console.ReadLine());
+            a = d / 100;
+            b = (d / 10) - a * 10;
+            c = d - ((d / 10) * 10);
+            Console.WriteLine($"Перше число= {a}");
+            Console.WriteLine($"Друге= {b}") ;
+            Console.WriteLine($"Третє= {c}");
+            sum = a+b+c;
+            Console.WriteLine($"Sum {sum}");
+            sum %= 2;
+            if (sum == 1)
+            {
+                Console.WriteLine("Ne parne");
+            }
+            else
+            {
+                Console.WriteLine("Parne");
+            }
+
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
---
+++ Lab4_Var9/Lab4_Var9/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Lab4_Var9
+{
+    class Program                        //Зсунути вправо
+    {
+        static void Main(string[] args)
+        {
+            
+           
+                Console.Write("Array size n = ");
+                var arr = new int[int.Parse(Console.ReadLine())];
+            Console.WriteLine("Введіть елементи ");
+                for (int i = 0; i < arr.Length; ++i)
+                    arr[i] = int.Parse(Console.ReadLine());
+
+                ShiftElements(arr);
+
+                
+            
+
+            static void ShiftElements(int[] arr)
+            {
+                int prev = arr[0];
+                int next;
+                for (int i = 0; i < arr.Length - 1; ++i)
+                {
+                    next = arr[i + 1];
+                    arr[i + 1] = prev;
+                    prev = next;
+                }
+                arr[0] = prev;
+            }
+
+            for (int i = 0; i < arr.Length; ++i)
+                Console.Write(arr[i]+" ");
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
